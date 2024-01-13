0d53eb1f3d49eff5418dad2b81ac797002e0e718
Denis Kulakov <kulakovdenis@mail.ru>
2024-01-14 02:15:18 +0300

    clear old files

--- generate_artchiapi.sh
+++
@@ -1,6 +0,0 @@
-MODULE="Modules/ArtInstituteChicagoNetworking/Sources/ArtInstituteChicagoNetworking"
-
-openapi-generator generate -i "Specifications/ArtInstituteChicagoSpecification.yml" -g swift5 -o "ArtInstituteChicagoApi"
-rm -r $MODULE""*
-cp -R "ArtInstituteChicagoApi/OpenAPIClient/Classes/OpenAPIs/". $MODULE
-rm -r "ArtInstituteChicagoApi"
--- generate_hamapi.sh
+++
@@ -1,6 +0,0 @@
-MODULE="Modules/HarvardArtMuseumsNetworking/Sources/HarvardArtMuseumsNetworking"
-
-openapi-generator generate -i "Specifications/HarvardArtMuseumSpecification.yml" -g swift5 -o "HarvardArtMuseumsApi"
-rm -r $MODULE""*
-cp -R "HarvardArtMuseumsApi/OpenAPIClient/Classes/OpenAPIs/". $MODULE
-rm -r "HarvardArtMuseumsApi"
